187ffd4f00e6235ec5169120d98709c45c50a4bb
Byungchan Gil <gilbc890@gmail.com>
2018-12-14 13:43:11 -0500

    lesson 7

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', storeController.homePage)
+
+// (req, res) => {
+//   const wes = { name:'wes', age:100, cool:true}
+//   // res.send('Hey! It works!');
+//   // res.json(wes)
+//   // res.send(req.query.name)
+//   // res.json(req.query)
+//   res.render('hello', {
+//     name:'wes',
+//     dog:req.query.dog,
+//     title:'i love food'
+//   });
+// });
+//
+//
+//   router.get('/reverse/:name', (req, res) => {
+//     const reverse = [...req.params.name].reverse().join('');
+//     res.send(reverse);
+//
+// });
 
 module.exports = router;
