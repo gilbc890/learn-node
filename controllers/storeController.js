da074150ce8f5516263741036b91217be130b573
Byungchan Gil <gilbc890@gmail.com>
2018-12-19 17:21:57 -0500

    lesson 17

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,18 @@ exports.getStores = async (req, res) => {
   console.log(stores)
   res.render('stores', {title:'Stores', stores});
 }
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id});
+  res.render('editStore', {title:`Edit ${store.name}`, store})
+}
+
+exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point'
+  const store = await Store.findOneAndUpdate({_id:req.params.id}, req.body, {
+    new:true,
+    runValidators:true
+  }).exec();
+  req.flash('success', `Successfully Updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+  res.redirect(`/stores/${store._id}/edit`);
+}
