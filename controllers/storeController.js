38e892ed5a6bcd6ca9867d3ee805ef83cdb869f2
Byungchan Gil <gilbc890@gmail.com>
2018-12-20 16:55:35 -0500

    lesson 21

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,41 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store')
+const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next){
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto){
+      next(null, true);
+    } else{
+      next({ message:"That filetype isn't allowed!"}, false)
+    }
+  }
+}
 
 exports.homePage = (req, res) => {
   res.render('index');
 }
 exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'})
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if(!req.file){
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800,jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 }
 
 exports.createStore = async (req, res) => {
@@ -33,4 +63,16 @@ exports.updateStore = async (req, res) => {
   }).exec();
   req.flash('success', `Successfully Updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
   res.redirect(`/stores/${store._id}/edit`);
+}
+
+exports.getStoreBySlug = async (req, res) => {
+  const store = await Store.findOne({ slug: req.params.slug});
+  if(!store) return next();
+  res.render('store', {store, title:store.name})
+}
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag
+  res.render('tag', {tags, title:'Tags', tag});
 }
