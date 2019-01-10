07f37f73702af0255dd91eecf3fcb8e2f4c0fd38
Byungchan Gil <gilbc890@gmail.com>
2019-01-10 18:12:33 -0500

    lesson 32

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,9 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
-
-
+/*
+API
+*/
+router.get('/api/search', catchErrors(storeController.searchStores));
 
 module.exports = router;
